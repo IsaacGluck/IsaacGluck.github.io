26383df62f0ce157b482a79d21f3b37b0a03d4af
IsaacGluck <isaacbgluck@gmail.com>
2015-12-18 20:24:33 -0500

    redirect

---
+++ js/redirect.js
@@ -0,0 +1,23 @@
+
+$("document").ready(function($) {
+	var timer = 5;
+	var string = "Redirecting in " + '<span class="animated flipInX">' + timer + '</span>' + "..."
+	$("#redirect").append(string);
+
+	function back(){
+		timer--;
+		$("#redirect").empty();
+		var string = "Redirecting in " + '<span class="animated flipInX">' + timer + '</span>' + "..."
+		$("#redirect").append(string);
+		if (timer==0){
+			clearInterval(refresh);
+			window.location.href = 'index.html';
+		}
+	}
+
+	var refresh = setInterval(function(){
+		back();
+	}, 1000);
+});
+
+
