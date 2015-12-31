118949bad7108d842da78f68053f778aa8d49581
IsaacGluck <isaacbgluck@gmail.com>
2015-12-31 04:17:08 -0500

    mobile size port

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('document').ready(function($) {
     $('#Contact').css('min-height', '700px');
     $('#Portfolio').css('min-height', '950px');
     if ($(window).width() < 992) {
-      $('#Portfolio').css('height', '1300px');
+      $('#Portfolio').css('height', '1500px');
       $('.mobile-message').removeClass('invisible'); // make this a check for mobile/tablet
       $('#About').css('height', 'auto');
     }
